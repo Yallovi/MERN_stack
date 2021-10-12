e2f81d0a201b42f142dd639657a11c70fd23c499
Yallovi <vivimoskalenko@yandex.ru>
2021-10-12 12:02:09 +0300

    auth for server path

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const Router = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {check, validationResult} = require('express-validator');
 
 const router = new Router();
@@ -26,17 +28,46 @@ router.post('/registration',
         const candidate = await User.findOne({email})
 
         if(candidate) {
-            return res.status(400).json({message: `User with email ${email} already exist`})
+            return res.status(400).json({message: `User with email ${email} already exist`});
         }
         // хэшируем пароль
-        const hashPassword = await bcrypt.hash(password, 15);
+        const hashPassword = await bcrypt.hash(password, 8);
 
         // если try создаем нового пользователя 
         const user = new User({email, password: hashPassword});
 
         // сохраняем в бд пользователя 
         await user.save();
-        return res.json({message: "User was created"})
+        return res.json({message: "User was created"});
+    }catch(e){
+        console.log(e);
+        res.send({message: 'Server error'});
+    }
+});
+
+router.post('/login',
+    async (req,res) => {
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message: 'User not found'});
+        }
+        const isPassValid = bcrypt.compareSync(password, user.password);
+        if(!isPassValid){
+            return res.status(400).json({message: 'Invalid password'});
+        }
+        const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"});
+        return res.json({
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                diskSpace: user.diskSpace,
+                usedSpace: user.usedSpace,
+                avater: user.avater,
+            }
+        })
     }catch(e){
         console.log(e);
         res.send({message: 'Server error'});
